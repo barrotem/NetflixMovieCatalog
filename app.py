23f38aa1ebfc19dc9eeee56a20dae69e897c87ce
BarRotem <bar.rotem45@gmail.com>
2024-09-11 20:57:05 +0300

    Return to http for Ansibel integration

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def status():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0', ssl_context=('cert.pem','key.pem'))
+    app.run(port=8080, host='0.0.0.0')#, ssl_context=('cert.pem','key.pem'))
